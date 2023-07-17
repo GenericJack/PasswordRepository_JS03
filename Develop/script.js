68572f9deec1eb438e3654b493ad2c41f295f626
John Simeoni <jack.simeoni@gmail.com>
2023-07-17 16:42:05 -0400

    tweaked function generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -2,39 +2,52 @@
 var generateBtn = document.querySelector("#generate");
 
 // Here we add a function to create a password
-function generatePassword(length, includeUpperCase, includeNumbers, includeSymbols)  {
-    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numberChars = '01123456789';
-    const symbolChars ='!@#$%^&*()-_=+[]{}|;:,.<>?';
+function generatePassword(length, includeUpperCase, includeNumbers, includeSymbols) {
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numberChars = '0123456789';
+  const symbolChars = '!@#$%^&*()-_=+[]{}|;:,.<>?';
 
-    let charset = lowercaseChars;
+  let charset = lowercaseChars;
 
-    if (includeUpperCase) {
-      charset += numberChars;
-    }
+  if (includeUpperCase) {
+    charset += uppercaseChars;
+  }
 
-    if (includeNumbers) {
-      charset += numberChars;
-    }
-    if (includeSymbols) {
-      charset += symbolChars;
-    }
+  if (includeNumbers) {
+    charset += numberChars;
+  }
 
-    if(charset.length === lowercaseChars.length) {
-      charset += uppercaseChars + numberChars + symbolChars;
-    }
-    let password ='';
-    const charsetLength = charset.length;
+  if (includeSymbols) {
+    charset += symbolChars;
+  }
 
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * charsetLength);
-      password =+ charset.charAt(randomIndex);
-    
-    }
+  if (charset.length === lowercaseChars.length) {
+    charset += uppercaseChars + numberChars + symbolChars;
+  }
 
-    return password;
-    }
+  let password = '';
+  const charsetLength = charset.length;
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charsetLength);
+    password += charset.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var length = 12; // Set your desired password length here
+  var includeUpperCase = true;
+  var includeNumbers = true;
+  var includeSymbols = true;
+
+  var password = generatePassword(length, includeUpperCase, includeNumbers, includeSymbols);
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
   
     // Write password to the #password input
 function writePassword() {
